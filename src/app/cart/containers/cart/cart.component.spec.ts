e3e935c7b74de282f679dccac519a0dcc48b9984
Patrick Roos <pa.roos@icloud.com>
2022-07-26 10:50:46 +0200

    Fix fleaky test

--- src/app/cart/containers/cart/cart.component.spec.ts
+++ src/app/cart/containers/cart/cart.component.spec.ts
@@ -1,35 +1,35 @@
-import {CartComponent} from './cart.component';
-import {Shallow} from "shallow-render";
-import {CartModule} from "../../cart.module";
-import {MockStore, provideMockStore} from "@ngrx/store/testing";
-import {initialState} from "../../reducers/cart.reducer";
-import {selectCartItems, selectCartTotalPrice} from "../../reducers/cart.selector";
-import {CartItem} from "../../model/cart-item.model";
-import {CartItemComponent} from "../../components/cart-item/cart-item.component";
-import {OrderByPipe} from "ngx-pipes";
-import {CurrencyPipe} from "@angular/common";
+import { CartComponent } from './cart.component';
+import { Shallow } from "shallow-render";
+import { CartModule } from "../../cart.module";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import { initialState } from "../../reducers/cart.reducer";
+import { selectCartItems, selectCartTotalPrice } from "../../reducers/cart.selector";
+import { CartItem } from "../../model/cart-item.model";
+import { CartItemComponent } from "../../components/cart-item/cart-item.component";
+import { OrderByPipe } from "ngx-pipes";
+import { CurrencyPipe } from "@angular/common";
 
 describe('CartComponent', () => {
   let shallow: Shallow<CartComponent>;
 
   beforeEach(async () => {
-    shallow = new Shallow<CartComponent>(CartComponent, CartModule).provideMock(provideMockStore({initialState}),);
+    shallow = new Shallow<CartComponent>(CartComponent, CartModule).provideMock(provideMockStore({ initialState }),);
   });
 
   it('should create', async () => {
-    const {fixture} = await shallow.render();
+    const { fixture } = await shallow.render();
     expect(fixture).toBeTruthy();
   });
 
   describe('cart', async () => {
     const selectedCartItems = [
-      {id: '1', item: {name: 'c'}} as CartItem,
-      {id: '2', item: {name: 'a'}} as CartItem,
-      {id: '3', item: {name: 'b'}} as CartItem
+      { id: '1', item: { name: 'c' } } as CartItem,
+      { id: '2', item: { name: 'a' } } as CartItem,
+      { id: '3', item: { name: 'b' } } as CartItem
     ];
 
     it(`should render ${selectedCartItems.length} CartItems`, async () => {
-      const {inject, fixture, findComponent} = await shallow.dontMock(OrderByPipe).render();
+      const { inject, fixture, findComponent } = await shallow.dontMock(OrderByPipe).render();
       const mockStore = inject(MockStore);
       mockStore.overrideSelector(selectCartItems, selectedCartItems);
       mockStore.refreshState();
@@ -39,21 +39,21 @@ describe('CartComponent', () => {
     });
 
     it('should order by name asc', async () => {
-      const {inject, fixture, findComponent} = await shallow.dontMock(OrderByPipe).render();
+      const { inject, fixture, findComponent } = await shallow.dontMock(OrderByPipe).render();
       const mockStore = inject(MockStore);
       mockStore.overrideSelector(selectCartItems, selectedCartItems);
       mockStore.refreshState();
       fixture.detectChanges();
 
       const cartItems = findComponent(CartItemComponent).map(n => n.cartItem)
-                                                        .map(n => n?.item)
-                                                        .map(n => n?.name);
+        .map(n => n?.item)
+        .map(n => n?.name);
 
       expect(cartItems).toEqual(['a', 'b', 'c']);
     });
 
-    it ('should show "empty cart" if there are not any CartItems', async () => {
-      const {find, inject, fixture} = await shallow.dontMock(OrderByPipe).render();
+    it('should show "empty cart" if there are not any CartItems', async () => {
+      const { find, inject, fixture } = await shallow.dontMock(OrderByPipe).render();
       const mockStore = inject(MockStore);
       mockStore.overrideSelector(selectCartItems, []);
       mockStore.refreshState();
@@ -66,7 +66,13 @@ describe('CartComponent', () => {
   describe('totalprice', () => {
     it('should show formatted', async () => {
       const expectedTotalPrice = 1234.50;
-      const {inject, fixture, find} = await shallow.dontMock(OrderByPipe).render();
+      const { inject, fixture, find } = await shallow.dontMock(OrderByPipe)
+        .provide(provideMockStore({
+          selectors: [
+            { selector: selectCartTotalPrice, value: expectedTotalPrice },
+            { selector: selectCartItems, value: [{}, {}, {}] }],
+        }))
+        .render();
       const mockStore = inject(MockStore);
       mockStore.overrideSelector(selectCartTotalPrice, expectedTotalPrice);
       mockStore.overrideSelector(selectCartItems, [{}, {}, {}]);
@@ -81,7 +87,7 @@ describe('CartComponent', () => {
   describe('close cart button', () => {
 
     it('should emit the close event', async () => {
-      const {find, inject, fixture, instance} = await shallow.dontMock(OrderByPipe).render();
+      const { find, inject, fixture, instance } = await shallow.dontMock(OrderByPipe).render();
       const mockStore = inject(MockStore);
       mockStore.overrideSelector(selectCartItems, []);
       mockStore.refreshState();
